5fa665f88011d21e0e146c4cd39604c39380397a
Samuel Shepard <vfn4@cdc.gov>
2019-07-29 13:32:32 -0400

    Updated post order script, tweaked main script.

--- LABEL_RES/scripts/findByNamePostOrder.pl
+++ LABEL_RES/scripts/findByNamePostOrder.pl
@@ -23,7 +23,7 @@
 # PROCESS parameters
 use File::Basename;
 use Getopt::Long;
-GetOptions( 'contains|C' => \$contains, 'directory-only|D' => \$dirOnly );
+GetOptions( 'contains|C' => \$contains, 'directory-only|D' => \$dirOnly, 'cat-print|P' => \$catPrint );
 
 if ( scalar(@ARGV) != 2 && !defined($dirOnly) ) {
 	$message = "Usage:\n\tperl $0 <base_directory> [<file/pattern> [-C]|-D]\n";
@@ -32,13 +32,26 @@ if ( scalar(@ARGV) != 2 && !defined($dirOnly) ) {
 	die($message);
 }
 
+$catPrint = defined($catPrint) ? 1 : 0;
+
+sub cat($) {
+	my $fh;
+	if ( !defined($_[0]) || $_[0] eq "" ) { return; }
+	open($fh,'<',"$_[0]") or die("Cannot open $_[0] for reading.\n");
+	while(my $line = <$fh>) {
+		print $line;
+	}
+	close($fh);
+	return 1;
+}
+
 #FNC - recursive post order traversal
 sub postOrderByName($$$) {
 	my $base = $_[0];
 	my $name = $_[1];
 	my $level = $_[2];
 
-	my @contents = <$base/*>;
+	my @contents = <"$base"/*>;
 	my @files = grep(-f $_, @contents);
 	my @dirs = grep(-d $_, @contents);
 	my $file;
@@ -52,7 +65,7 @@ sub postOrderByName($$$) {
 
 	if ( $dirOnly ) {
 		foreach $dir ( @dirs ) {
-			print $dir,"\n";
+			print '"',$dir,"\"\n";
 		}
 		return;
 	}
@@ -62,10 +75,14 @@ sub postOrderByName($$$) {
 		# Process
 		foreach $file ( @files ) {
 			if ( basename($file) =~ /$name/ ) {
-				if ( $level != 0 || $file ne $files[-1] ) {
-					print $file,' ';
+				if ( $catPrint ) {
+					cat($file) or die("Can't cat $file: $!");
 				} else {
-					print $file;
+					if ( $level != 0 || $file ne $files[-1] ) {
+						print '"',$file,'" ';
+					} else {
+						print '"',$file,'"';
+					}
 				}
 			}
 		}
@@ -74,10 +91,14 @@ sub postOrderByName($$$) {
 		# Process
 		foreach $file ( @files ) {
 			if ( basename($file) eq $name ) {
-				if ( $level != 0 || $file ne $files[-1] ) {
-					print $file,' ';
+				if ( $catPrint ) {
+					cat($file) or die("Can't cat $file: $!");
 				} else {
-					print $file;
+					if ( $level != 0 || $file ne $files[-1] ) {
+						print '"',$file,'" ';
+					} else {
+						print '"',$file,'"';
+					}
 				}
 			}
 		}
